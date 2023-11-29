17c91e3f6410e1a744a91f40d326cd5787733add
신준혁 <oridori2705@naver.com>
2023-11-29 18:01:29 +0900

    commit: documents 출력 및 컴포넌트 재귀호출 활용

---
+++ src/components/LNB.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav>
+    <div class="header">
+      <div class="user_profile"></div>
+      Leon's Notion
+    </div>
+    <ul>
+      <WorkspaceItem
+        v-for="workspace in workspaces"
+        :key="workspace.id"
+        :workspace="workspace"
+      />
+    </ul>
+    <div class="actions"></div>
+  </nav>
+</template>
+
+<script>
+import WorkspaceItem from "~/components/WorkspaceItem";
+
+export default {
+  components: {
+    WorkspaceItem,
+  },
+  computed: {
+    workspaces() {
+      return this.$store.state.workspace.workspaces;
+    },
+  },
+  created() {
+    this.$store.dispatch("workspace/readWorkspaces");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 240px;
+  height: 100%;
+  background-color: $color-background;
+  .header {
+    padding: 14px;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    .user_profile {
+      width: 20px;
+      height: 20px;
+      border-radius: 4px;
+      margin-right: 10px;
+      background-image: url("/static/icon.jpg");
+      background-size: cover;
+    }
+  }
+  ul {
+  }
+  .actions {
+  }
+}
+</style>
---
+++ src/components/WorkspaceItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <div :style="{ paddingLeft: `${14 * depth}px` }" class="title">
+      {{ workspace.title }}
+    </div>
+    <ul v-if="hasChildren">
+      <WorkspaceItem
+        v-for="ws in workspace.documents"
+        :key="ws.id"
+        :workspace="ws"
+        :depth="depth + 1"
+      />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    workspace: {
+      type: Object,
+      default: () => ({}),
+    },
+    depth: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    hasChildren() {
+      return this.workspace.documents && this.workspace.documents.length;
+    },
+  },
+};
+</script>
+
+<style></style>
